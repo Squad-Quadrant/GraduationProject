15369873c41bc59109c0cea48cc3ab4731b13349
wuzehua <hmxsqaq@gmail.com>
2025-11-15 01:02:16 +0800

    [chore] add example event

---
+++ Assets/Scripts/Data/Runtime/Events/ExampleEvent.cs
@@ -0,0 +1,11 @@
+﻿using Core.Events;
+
+namespace Data.Runtime.Events
+{
+	public struct ExampleEvent : IEvent
+	{
+		public string Msg { get; }
+
+		public ExampleEvent(string msg) => Msg = msg;
+	}
+}
