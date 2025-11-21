8139eb94ef17c6e86cc486c7d0e49e68e0acd708
wuzehua <hmxsqaq@gmail.com>
2025-11-21 23:43:32 +0800

    [feat] refactor unit creation and destruction events

---
+++ Assets/Scripts/Data/Runtime/Events/UnitCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Core.Events;
+using Systems.Unit;
+
+namespace Data.Runtime.Events
+{
+	public readonly struct UnitCreatedEvent : IEvent
+	{
+		public Unit Unit { get; }
+
+		public UnitCreatedEvent(Unit unit) => Unit = unit;
+
+		public override string ToString() => $"[UnitCreated] {Unit.name}({Unit.Id}) at {Unit.position}";
+	}
+}
--- Assets/Scripts/Data/Runtime/Events/UnitDestroyedEvent.cs
+++ Assets/Scripts/Data/Runtime/Events/UnitDestroyedEvent.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 namespace Data.Runtime.Events
 {
-	public readonly struct UnitCreatedEvent : IEvent
-	{
-		public Unit Unit { get; }
-
-		public UnitCreatedEvent(Unit unit) => Unit = unit;
-
-		public override string ToString() => $"[UnitCreated] {Unit.name}({Unit.Id}) at {Unit.position}";
-	}
-
 	public readonly struct UnitDestroyedEvent : IEvent
 	{
 		public Unit Unit { get; }
