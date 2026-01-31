b3dbffe157bc0e14d3587021b04213745e09a7b0
wuzehua <hmxsqaq@gmail.com>
2026-02-01 05:31:48 +0800

    [test] update spine assets

--- Assets/Test/WZHTest/Test.cs
+++ Assets/Test/WZHTest/Test.cs
@@ -2,6 +2,7 @@
 using Presentation.Bootstrap;
 using Presentation.UI.Core;
 using Sirenix.OdinInspector;
+using Spine;
 using Spine.Unity;
 using UnityEngine;
 
@@ -10,11 +11,24 @@ namespace Test.WZHTest
 	public class Test : MonoBehaviour
 	{
 		[SerializeField] private SkeletonAnimation skeletonAnimation;
+		[SerializeField] private string characterSkinName = "pcb front";
+		[SerializeField] private string weaponSkinName = "weapon_a1";
 
 		[Button]
 		private void Foo()
 		{
-			skeletonAnimation.Skeleton.SetAttachment("BG", null);
+			var combinedSkin = new Skin("combined-skin");
+			var characterSkin = skeletonAnimation.Skeleton.Data.FindSkin(characterSkinName);
+			var weaponSkin = skeletonAnimation.Skeleton.Data.FindSkin(weaponSkinName);
+			if (characterSkin == null || weaponSkin == null)
+			{
+				Debug.LogError("One or both skins not found.");
+				return;
+			}
+			combinedSkin.AddSkin(characterSkin);
+			combinedSkin.AddSkin(weaponSkin);
+			skeletonAnimation.Skeleton.SetSkin(combinedSkin);
+			skeletonAnimation.Skeleton.SetSlotsToSetupPose();
 		}
 	}
 }
