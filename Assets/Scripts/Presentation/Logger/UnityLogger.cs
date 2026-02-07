3a75b1108e7ad3b91b03ba4857b66fa7378d4cc6
紫地丁 <117638490+Pditine@users.noreply.github.com>
2026-02-07 16:21:00 +0800

    [feat] Logger加颜色

--- Assets/Scripts/Presentation/Logger/UnityLogger.cs
+++ Assets/Scripts/Presentation/Logger/UnityLogger.cs
@@ -1,6 +1,9 @@
 using System;
 using Core.Log;
 using Data.Config;
+using UnityEngine;
+using Color = UnityEngine.Color;
+using ILogger = Core.Log.ILogger;
 
 namespace Presentation.Logger
 {
@@ -39,6 +42,23 @@ namespace Presentation.Logger
 				UnityEngine.Debug.LogError(format ? FormatMessage(message) : message);
 		}
 
-		private string FormatMessage(string message) => $"[{_type.Name}] {message}";
+		// private string FormatMessage(string message) => $"[{_type.Name}] {message}";
+        private string FormatMessage(string message, LogLevel level = LogLevel.Debug)
+        {
+            var color = GetLogLevelColor(level);
+            return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>[{_type.Name}]</color> {message}";
+        }
+        
+        public static Color GetLogLevelColor(LogLevel level)
+        {
+            return level switch
+            {
+                LogLevel.Debug => Color.green,
+                LogLevel.Info => Color.cyan,
+                LogLevel.Warning => Color.yellow,
+                LogLevel.Error => Color.red,
+                _ => Color.white
+            };
+        }
 	}
 }
