20af283e4a3b430a45da6dfbf89cf9edc9c9d269
紫地丁 <117638490+Pditine@users.noreply.github.com>
2026-02-28 02:07:24 +0800

    [feat] temp

--- Assets/Scripts/Presentation/UI/Panel/TurnPanel.cs
+++ Assets/Scripts/Presentation/UI/Panel/TurnPanel.cs
@@ -1,6 +1,8 @@
 using System;
 using Data.Runtime.Events.Turn;
+using Data.Runtime.Events.View;
 using Presentation.UI.Core;
+using PurpleFlowerCore.Utility;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,7 +16,13 @@ namespace Presentation.UI.Panel
 
         protected override void OnInitialize()
         {
-            nextUnitButton.onClick.AddListener(() => EventBus.Publish(new UnitTurnEndedEvent()));
+            nextUnitButton.onClick.AddListener(() => { 
+                EventBus.Publish(new UnitTurnEndedEvent());
+                DelayUtility.Delay(1f, () =>
+                {
+                    EventBus.Publish(new PresentationCompleteEvent(EPresentationCategory.UI, PresentationType.UI.TurnBanner));
+                });
+            });
             nextTurnButton.onClick.AddListener(() => EventBus.Publish(new TurnEndedEvent()));
 
         }
