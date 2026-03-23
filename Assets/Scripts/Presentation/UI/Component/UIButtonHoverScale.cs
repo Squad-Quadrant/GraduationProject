fcd41cce5569848fc1fa8bcdf1d29a65533a4a4f
wuzehua <hmxsqaq@gmail.com>
2026-03-23 19:31:59 +0800

    [feat] action menu optimize

---
+++ Assets/Scripts/Presentation/UI/Component/UIButtonHoverScale.cs
@@ -0,0 +1,127 @@
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Presentation.UI.Component
+{
+	[RequireComponent(typeof(Button))]
+	public class UIButtonHoverScale : MonoBehaviour,
+		IPointerEnterHandler,
+		IPointerExitHandler,
+		IPointerUpHandler,
+		IPointerDownHandler
+	{
+		[FoldoutGroup("To Max (Hover)")] [Range(1f, 1.3f)]
+		[SerializeField] private float maxScale = 1.1f;
+
+		[FoldoutGroup("To Max (Hover)")] [Range(0.02f, 0.5f)] [SerializeField]
+		private float toMaxDuration = 0.15f;
+
+		[FoldoutGroup("To Max (Hover)")] [SerializeField]
+		private Ease toMaxEase = Ease.OutBack;
+
+		[FoldoutGroup("To Min (Press)")] [Range(0.5f, 1f)] [SerializeField]
+		private float minScale = 0.85f;
+
+		[FoldoutGroup("To Min (Press)")] [Range(0.01f, 0.15f)] [SerializeField]
+		private float toMinDuration = 0.06f;
+
+		[FoldoutGroup("To Min (Press)")] [SerializeField]
+		private Ease toMinEase = Ease.OutQuad;
+
+		[FoldoutGroup("To Origin (Idle)")] [Range(0.02f, 0.5f)] [SerializeField]
+		private float toOriginDuration = 0.12f;
+
+		[FoldoutGroup("To Origin (Idle)")] [SerializeField]
+		private Ease toOriginEase = Ease.OutQuad;
+
+		// --- Runtime ---
+		private Vector3 _originalScale;
+		private Tween _currentTween;
+		private Button _button;
+		private bool _isHovered;
+		private bool _isPressed;
+
+		private void Awake()
+		{
+			_originalScale = transform.localScale;
+			_button = GetComponent<Button>();
+		}
+
+		// --- Pointer events: set/clear flags, then let ApplyState decide ---
+
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			_isHovered = true;
+			ApplyState();
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			_isHovered = false;
+			ApplyState();
+		}
+
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			_isPressed = true;
+			ApplyState();
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			_isPressed = false;
+			ApplyState();
+		}
+
+		// --- Core: pick a target based on current flags ---
+
+		private void ApplyState()
+		{
+			if (!_button.interactable)
+			{
+				KillTween();
+				transform.localScale = _originalScale;
+				return;
+			}
+
+			// Priority: Pressed > Hovered > Idle
+			if (_isPressed)
+				AnimateTo(_originalScale * minScale, toMinDuration, toMinEase);
+			else if (_isHovered)
+				AnimateTo(_originalScale * maxScale, toMaxDuration, toMaxEase);
+			else
+				AnimateTo(_originalScale, toOriginDuration, toOriginEase);
+		}
+
+		private void AnimateTo(Vector3 target, float duration, Ease ease)
+		{
+			KillTween();
+
+			_currentTween = transform
+				.DOScale(target, duration)
+				.SetEase(ease)
+				.SetAutoKill(false)
+				.SetUpdate(true)
+				.SetLink(gameObject);
+		}
+
+		private void KillTween()
+		{
+			if (_currentTween != null && _currentTween.IsActive())
+				_currentTween.Kill();
+
+			_currentTween = null;
+		}
+
+		private void OnDisable()
+		{
+			KillTween();
+			transform.localScale = _originalScale;
+			_isHovered = false;
+			_isPressed = false;
+		}
+	}
+}
--- Assets/Scripts/Presentation/UI/Panel/ActionMenuPanel.cs
+++ Assets/Scripts/Presentation/UI/Panel/ActionMenuPanel.cs
@@ -1,45 +1,65 @@
 ﻿using Data.Runtime;
-using Data.Runtime.Events.Interaction;
 using Data.Runtime.Events.UI;
+using Presentation.UI.Component;
 using Presentation.UI.Core;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Presentation.UI.Panel
 {
 	public class ActionMenuPanel : UIPanel, IInitializable<Systems.Unit.Unit>
 	{
-		[SerializeField] private Button moveButton;
-		[SerializeField] private Button mainWeaponButton;
-		[SerializeField] private Button secondaryWeaponButton;
-		[SerializeField] private Button TacticalItem0Button;
-		[SerializeField] private Button TacticalItem1Button;
-		[SerializeField] private Button TacticalItem2Button;
-		[SerializeField] private Button waitButton;
+		[SerializeField, Required] private ActionBtn moveButton;
+		[SerializeField, Required] private ActionBtn mainWeaponButton;
+		[SerializeField, Required] private ActionBtn secondaryWeaponButton;
+		[SerializeField, Required] private ActionBtn tacticalItem0Button;
+		[SerializeField, Required] private ActionBtn tacticalItem1Button;
+		[SerializeField, Required] private ActionBtn tacticalItem2Button;
+		[SerializeField, Required] private ActionBtn waitButton;
+		[SerializeField, Required] private TextMeshProUGUI currentActionText;
 
         // todo: 复杂化该UI的逻辑
 		protected override void OnInitialize()
 		{
-			moveButton?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.Move)));
-            mainWeaponButton?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.MainWeapon)));
-            secondaryWeaponButton?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.SecondaryWeapon)));
-            TacticalItem0Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem0)));
-            TacticalItem1Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem1)));
-            TacticalItem2Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem2)));
-            waitButton?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.Wait)));
+			moveButton?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.Move)));
+            mainWeaponButton?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.MainWeapon)));
+            secondaryWeaponButton?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.SecondaryWeapon)));
+            tacticalItem0Button?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem0)));
+            tacticalItem1Button?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem1)));
+            tacticalItem2Button?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.TacticalItem2)));
+            waitButton?.Button?.onClick.AddListener(() => EventBus.Publish(new ActionSelectedEvent(EActionType.Wait)));
+
+            moveButton!.OnHoverEnter += SetDescription;
+            moveButton.OnHoverExit += ClearDescription;
+            mainWeaponButton!.OnHoverEnter += SetDescription;
+            mainWeaponButton.OnHoverExit += ClearDescription;
+            secondaryWeaponButton!.OnHoverEnter += SetDescription;
+            secondaryWeaponButton.OnHoverExit += ClearDescription;
+            tacticalItem0Button!.OnHoverEnter += SetDescription;
+            tacticalItem0Button.OnHoverExit += ClearDescription;
+            tacticalItem1Button!.OnHoverEnter += SetDescription;
+            tacticalItem1Button.OnHoverExit += ClearDescription;
+            tacticalItem2Button!.OnHoverEnter += SetDescription;
+            tacticalItem2Button.OnHoverExit += ClearDescription;
+            waitButton!.OnHoverEnter += SetDescription;
+            waitButton.OnHoverExit += ClearDescription;
 		}
 
 		public void DataInitialize(Systems.Unit.Unit unit)
 		{
             var availableActions = unit.GetAvailableActions();
 
-            moveButton.interactable = availableActions.Contains(EActionType.Move);
-            mainWeaponButton.interactable = availableActions.Contains(EActionType.MainWeapon);
-            secondaryWeaponButton.interactable = availableActions.Contains(EActionType.SecondaryWeapon);
-            TacticalItem0Button.interactable = availableActions.Contains(EActionType.TacticalItem0);
-            TacticalItem1Button.interactable = availableActions.Contains(EActionType.TacticalItem1);
-            TacticalItem2Button.interactable = availableActions.Contains(EActionType.TacticalItem2);
-			waitButton.interactable = availableActions.Contains(EActionType.Wait);
+            moveButton.Button.interactable = availableActions.Contains(EActionType.Move);
+            mainWeaponButton.Button.interactable = availableActions.Contains(EActionType.MainWeapon);
+            secondaryWeaponButton.Button.interactable = availableActions.Contains(EActionType.SecondaryWeapon);
+            tacticalItem0Button.Button.interactable = availableActions.Contains(EActionType.TacticalItem0);
+            tacticalItem1Button.Button.interactable = availableActions.Contains(EActionType.TacticalItem1);
+            tacticalItem2Button.Button.interactable = availableActions.Contains(EActionType.TacticalItem2);
+			waitButton.Button.interactable = availableActions.Contains(EActionType.Wait);
         }
+
+		private void SetDescription(string desc) => currentActionText.text = desc;
+		private void ClearDescription(string _) => currentActionText.text = "";
 	}
 }
