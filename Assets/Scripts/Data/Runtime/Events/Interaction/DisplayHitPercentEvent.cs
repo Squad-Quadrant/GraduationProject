713c2845c2bc53dcb609af6dbf2458d28e34b1b7
wuzehua <hmxsqaq@gmail.com>
2026-04-09 00:18:12 +0800

    [feat] limit movement ap cost in 1 turn & optimize interaction context code structure

---
+++ Assets/Scripts/Data/Runtime/Events/Interaction/DisplayHitPercentEvent.cs
@@ -0,0 +1,19 @@
+﻿using Core.Events;
+
+namespace Data.Runtime.Events.Interaction
+{
+	public readonly struct DisplayHitPercentEvent : IEvent
+	{
+		public readonly bool IsValid;
+		public readonly int HitPercent;
+
+		private DisplayHitPercentEvent(int hitPercent, bool isValid)
+		{
+			HitPercent = hitPercent;
+			IsValid = isValid;
+		}
+
+		public static DisplayHitPercentEvent Valid(int hitPercent) => new(hitPercent, true);
+		public static DisplayHitPercentEvent Invalid() => new(0, false);
+	}
+}
