7ea655c975fba49ef11eca51d10ba6a76b430c1a
紫地丁 <117638490+Pditine@users.noreply.github.com>
2026-06-21 23:07:39 +0800

    [fix] 部位概率

--- Assets/Scripts/Systems/Damage/DamageInfluence/BodyDestructionInfluence.cs
+++ Assets/Scripts/Systems/Damage/DamageInfluence/BodyDestructionInfluence.cs
@@ -29,7 +29,7 @@ namespace Systems.Damage
     {
         public static Dictionary<BodyPartType, float> Rate = new()
         {
-            { BodyPartType.Legs, 111.15f },
+            { BodyPartType.Legs, 0.15f },
             { BodyPartType.Arms, 0.13f },
             { BodyPartType.Head, 0.08f },
             { BodyPartType.Torso, 0.64f },
