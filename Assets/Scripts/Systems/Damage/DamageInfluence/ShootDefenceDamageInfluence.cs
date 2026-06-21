dc32a0cc984293d5693357728bd5145ac79c4bbe
紫地丁 <117638490+Pditine@users.noreply.github.com>
2026-06-22 00:45:02 +0800

    [fix] 护甲伤害也要添加距离衰减

--- Assets/Scripts/Systems/Damage/DamageInfluence/ShootDefenceDamageInfluence.cs
+++ Assets/Scripts/Systems/Damage/DamageInfluence/ShootDefenceDamageInfluence.cs
@@ -8,6 +8,7 @@ namespace Systems.Damage
 {
     public class ShootDefenceDamageInfluence : DamageInfluence
     {
+        public Unit.Unit UnitAttacker => Attacker as Unit.Unit;
         public ShootDefenceDamageInfluence(IDamageInfluencer owner, int priority = 1) : base(owner, priority)
         {
         }
@@ -17,7 +18,22 @@ namespace Systems.Damage
         public override void Execute()
         {
             var theWeapon = (WeaponLogic)Owner;
-            int defenceDamage = Mathf.FloorToInt(theWeapon.GetDamage() * (1 - Defender.defenseRate) * (1 - theWeapon.PenetrationRate())); 
+            
+            // 获得两个单位的距离
+            float distance = Mathf.Abs(UnitAttacker.position.x - Defender.position.x) + Mathf.Abs(UnitAttacker.position.y - Defender.position.y);
+            
+            // 伤害衰减
+            float damageMultiplier = 1f;
+
+            var damageAttenuation = theWeapon.DamageAttenuation();
+
+            // 每()格衰减()
+            int power = Mathf.FloorToInt(distance / damageAttenuation.perGrid);
+            damageMultiplier *= Mathf.Pow(damageAttenuation.multiplier, power);
+            
+            int damage = Mathf.FloorToInt(((WeaponLogic)Owner).GetDamage() * damageMultiplier);
+            
+            int defenceDamage = Mathf.FloorToInt(damage * (1 - Defender.defenseRate) * (1 - theWeapon.PenetrationRate())); 
             
             Context.DefenceDamage += defenceDamage;
         }
